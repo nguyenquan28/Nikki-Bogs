c48dad3cf4e9530944b4abd86fca0247e83699d7
nguyenquan28 <nguyenquan.cntt.k17da@gmail.com>
2020-09-08 22:47:33 +0700

    chat

--- controller/chatController.php
+++ controller/chatController.php
@@ -5,6 +5,19 @@ class chatController
 {
     function getAll()
     {
+        
+        $chat = new chatModel();
+        if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 10;
+        $offset = ($pageno - 1) * $no_of_records_per_page;
+        $data = $chat->getAll($offset, $no_of_records_per_page)->fetch_all(1);
+        $total_pages = $chat->paginasion($no_of_records_per_page);
+
+        // print_r($data);
         require_once __DIR__ . '../../views/admin/chat.php';
     }
 }
---
+++ model/chat.php
@@ -0,0 +1,65 @@
+<?php
+require_once __DIR__ . '/../config/session.php';
+Session::init();
+require_once __DIR__ . '/../config/connectDB.php';
+require_once __DIR__ . '/../config/format.php';
+
+class chat
+{
+    public $chat_id;
+    public $receiver_id;
+    public $sender_id;
+    public $message;
+
+    function __construct($chat_id, $receiver_id, $sender_id, $message)
+    {
+        $this->chat_id = $chat_id;
+        $this->receiver_id = $receiver_id;
+        $this->sender_id = $sender_id;
+        $this->message = $message;
+    }
+}
+
+class chatModel
+{
+    private $db;
+    private $fm;
+
+    function __construct()
+    {
+        $this->db = new Database();
+        $this->fm = new Format();
+    }
+
+    // Function paginasion
+    function paginasion($no_of_records_per_page)
+    {
+        $total_pages_sql = "SELECT COUNT(*) FROM chat";
+        $result = $this->db->select($total_pages_sql);
+
+        $total_rows = mysqli_fetch_array($result)[0];
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+        return $total_pages;
+    }
+
+    // get all data in table user
+    function getAll($offset, $no_of_records_per_page)
+    {
+
+        $query = "SELECT * FROM chat ORDER BY status DESC LIMIT $offset, $no_of_records_per_page";
+        $result = $this->db->select($query);
+
+        return $result;
+    }
+
+
+    // Get name user by id
+    function getName($chat_id)
+    {
+        $query = "SELECT name FROM chat WHERE chat_id = '$chat_id' ";
+        $data = $this->db->select($query);
+        $result = $data->fetch_assoc();
+        // $cat = new user($result[0],$result[1],$result[2],$result[3],$result[4],$result[5]);
+        return $result;
+    }
+}
